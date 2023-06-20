6ebe26c940ce4d2af75a96606397953d2c87ff87
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2023-06-20 09:16:44 -0400

    Update example.py

--- debug/example.py
+++ debug/example.py
@@ -2,6 +2,7 @@
 
 from acore_soap_app.agent.soap_agent import run_soap_command
 
-command = "server info"
+# command = "server info"
+command = ".account onlinelist"
 response = run_soap_command(command=command)
 print(response)
