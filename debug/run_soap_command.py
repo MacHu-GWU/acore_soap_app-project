11b8451fcc54f79fd9ab2a7aded1280f24c8f400
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2023-06-21 10:25:00 -0400

    Update run_soap_command.py

--- debug/run_soap_command.py
+++ debug/run_soap_command.py
@@ -4,9 +4,9 @@
 这个脚本用于测试在 EC2 实例上运行 Soap 命令. (只能在 EC2 上运行, 不能在本地电脑上运行)
 """
 
-from acore_soap_app.agent.api import run_soap_command
+from acore_soap_app.agent.api import SOAPRequest
 
-# command = "server info"
-command = ".account onlinelist"
-soap_res = run_soap_command(command=command)
-soap_res.print()
+command = "server info"
+# command = ".account onlinelist"
+response = SOAPRequest(command=command).send()
+response.print()
