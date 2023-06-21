a75c5bf6334b866e4f08ba58e15d57444b98e59c
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2023-06-21 09:48:14 -0400

    refactor cli

--- acore_soap_app/cli/impl.py
+++ acore_soap_app/cli/impl.py
@@ -1,11 +1,9 @@
 # -*- coding: utf-8 -*-
 
 import typing as T
-import json
 
 from ..agent.api import SOAPRequest, SOAPResponse, get_boto_ses
 from ..exc import SOAPCommandFailedError
-from ..utils import get_object, put_object
 
 
 def ensure_ec2_environment():
@@ -31,18 +29,21 @@ def ensure_response_succeeded(
 
 
 def gm(
-    request: T.Union[str, SOAPRequest],
+    request_like: T.Union[
+        str,
+        T.List[str],
+        "SOAPRequest",
+        T.List["SOAPRequest"],
+    ],
     username: T.Optional[str] = None,
     password: T.Optional[str] = None,
     raises: bool = True,
     s3uri_output: T.Optional[str] = None,
 ):
     """
-    运行一个 GM 命令, 例如 ``.server info``.
+    运行一个或多个 GM 命令. 例如 ``.server info``.
 
-    :param request: 如果是 :class:`~acore_soap_app.agent.impl.SOAPRequest` 对象,
-        那么就直接发送该请求. 如果是一个字符串, 则将其视为一个 S3 URI, 到 S3 上读取请求
-        对象数据. 常用于请求特别大的情况.
+    :param request_like: 请参考 :class:`~acore_soap_app.agent.impl.SOAPRequest.batch_load`
     :param username: 默认的用户名, 只有当 request.username 为 None 的时候才会用到.
     :param password: 默认的密码, 只有当 request.password 为 None 的时候才会用到.
     :param raises: 默认为 True. 如果为 True, 则在遇到错误时抛出异常. 反之则将
@@ -52,62 +53,20 @@ def gm(
         如果给定, 则将 JSON 保存到 S3 中. 常用于返回结果特别大的情况.
     """
     ensure_ec2_environment()
-    s3_client = None
 
     # handle input
-    if isinstance(request, str):
-        boto_ses = get_boto_ses()
-        s3_client = boto_ses.client("s3")
-        request = SOAPRequest.from_json(get_object(s3_client, s3uri=request))
-    request.set_default(username=username, password=password)
-
-    # run
-    response = ensure_response_succeeded(request, request.send(), raises=raises)
-
-    # handle output
-    if s3uri_output is None:
-        print(response.to_json())
-    else:
-        if s3_client is None:
-            boto_ses = get_boto_ses()
-            s3_client = boto_ses.client("s3")
-        put_object(s3_client, s3uri=s3uri_output, body=response.to_json())
-
-
-def batch_gm(
-    requests: T.Union[str, T.Iterable[SOAPRequest]],
-    username: T.Optional[str] = None,
-    password: T.Optional[str] = None,
-    raises: bool = True,
-    s3uri_output: T.Optional[str] = None,
-):
-    """
-    按顺序运行一批 GM 命令.
-
-    :param requests: 一批 :class:`~acore_soap_app.agent.impl.SOAPRequest` 对象.
-    :param username: 默认的用户名, 只有当 request.username 为 None 的时候才会用到.
-    :param password: 默认的密码, 只有当 request.password 为 None 的时候才会用到.
-    :param raises: 默认为 True. 如果为 True, 则在遇到错误时抛出异常. 反之则将
-        failed SOAP Response 原封不动地返回.
-    :param s3uri_output: 可选参数, 如果为 None, 则将
-        :class:`~acore_soap_app.agent.impl.SOAPResponse` 对象转换为 JSON 并打印.
-        如果给定, 则将 JSON 保存到 S3 中. 常用于返回结果特别大的情况.
-    """
-    ensure_ec2_environment()
-    s3_client = None
-
-    # handle input
-    if isinstance(requests, str):
-        boto_ses = get_boto_ses()
-        s3_client = boto_ses.client("s3")
-        json_str = get_object(s3_client, s3uri=requests)
-        requests = [SOAPRequest.from_dict(dct) for dct in json.loads(json_str)]
-    for request in requests:
-        request.set_default(username=username, password=password)
+    boto_ses = get_boto_ses()
+    s3_client = boto_ses.client("s3")
+    requests = SOAPRequest.batch_load(
+        request_like=request_like,
+        username=username,
+        password=password,
+        s3_client=s3_client,
+    )
 
     # run
     responses = [
-        ensure_response_succeeded(request, request.send(), raises)
+        ensure_response_succeeded(request, request.send(), raises=raises)
         for request in requests
     ]
 
@@ -115,40 +74,8 @@ def batch_gm(
     if s3uri_output is None:
         print("\n".join([response.to_json() for response in responses]))
     else:
-        if s3_client is None:
-            boto_ses = get_boto_ses()
-            s3_client = boto_ses.client("s3")
-        put_object(
-            s3_client,
+        SOAPResponse.batch_dump_to_s3(
+            s3_client=s3_client,
+            instances=responses,
             s3uri=s3uri_output,
-            body=json.dumps([response.to_dict() for response in responses]),
         )
-
-
-# def remote_gm(
-#     server_id: str,
-#     cmd: str,
-#     username: str = "admin",
-#     password: str = "admin",
-#     region: T.Optional[str] = None,
-#     profile: T.Optional[str] = None,
-#     delays: int = 1,
-#     timeout: int = 10,
-#     verbose: bool = True,
-# ):
-#     """
-#     Example: acsoap gm ".server info"
-#     """
-#     bsm = BotoSesManager(region_name=region, profile_name=profile)
-#     soap_res = run_remote_soap_command(
-#         bsm=bsm,
-#         server_id=server_id,
-#         cmd=cmd,
-#         username=username,
-#         password=password,
-#         sync=True,
-#         delays=delays,
-#         timeout=timeout,
-#         verbose=verbose,
-#     )
-#     print(soap_res.to_json())
--- acore_soap_app/cli/main.py
+++ acore_soap_app/cli/main.py
@@ -1,24 +1,11 @@
 # -*- coding: utf-8 -*-
 
 import typing as T
-import json
 import fire
-from boto_session_manager import BotoSesManager
 
 from .impl import (
     gm,
-    batch_gm,
 )
-from ..agent.api import (
-    SOAPRequest,
-    SOAPResponse,
-    get_boto_ses,
-)
-
-
-def ensure_ec2_environment():
-    if not __file__.startswith("/home/ubuntu"):
-        raise EnvironmentError("This is not a EC2 environment")
 
 
 class Command:
@@ -53,83 +40,13 @@ class Command:
 
         - acsoap gm ".server info" --s3uri s3://bucket/output.json
         """
-        if cmd.startswith("s3://"):
-            request = cmd
-        else:
-            request = SOAPRequest(command=cmd, username=user, password=pwd)
         gm(
-            request=request,
+            request_like=cmd,
             username=user,
             password=pwd,
             raises=raises,
             s3uri_output=s3uri,
         )
-
-    def batch_gm(
-        self,
-        s3uri_in: str,
-        user: T.Optional[str] = None,
-        pwd: T.Optional[str] = None,
-        raises: bool = True,
-        s3uri_out: T.Optional[str] = None,
-    ):
-        """
-        Run sequence of GM commands.
-
-        :param s3uri_in: the S3 URI of the JSON file that contains the soap requests,
-            example: ``[{"command": ".account create test1 ..."}, {"command": ".account create test2 ..."}]``
-        :param user: in game GM account username, if not given, then use "admin"
-        :param pwd: in game GM account password, if not given, then use "admin"
-        :param raises: raise error if any of the GM command failed.
-        :param s3uri_out: if None, then return the response as JSON, otherwise, save
-            the response to S3.
-
-        Example::
-
-        - acsoap batch_gm --help
-
-        - acsoap batch_gm s3://bucket/input.json
-
-        - acsoap batch_gm s3://bucket/input.json --user myuser --pwd mypwd
-
-        - acsoap batch_gm s3://bucket/input.json --s3uri_out s3://bucket/output.json
-        """
-        batch_gm(
-            requests=s3uri_in,
-            username=user,
-            password=pwd,
-            raises=raises,
-            s3uri_output=s3uri_out,
-        )
-
-    # def remote_gm(
-    #     self,
-    #     server_id: str,
-    #     cmd: str,
-    #     username: str = "admin",
-    #     password: str = "admin",
-    #     region: T.Optional[str] = None,
-    #     profile: T.Optional[str] = None,
-    #     delays: int = 1,
-    #     timeout: int = 10,
-    #     verbose: bool = True,
-    # ):
-    #     """
-    #     Example: acsoap gm ".server info"
-    #     """
-    #     bsm = BotoSesManager(region_name=region, profile_name=profile)
-    #     soap_res = run_remote_soap_command(
-    #         bsm=bsm,
-    #         server_id=server_id,
-    #         cmd=cmd,
-    #         username=username,
-    #         password=password,
-    #         sync=True,
-    #         delays=delays,
-    #         timeout=timeout,
-    #         verbose=verbose,
-    #     )
-    #     print(soap_res.to_json())
 
 
 def run():
