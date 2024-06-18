670de1a8384ac7c65f1a0a68934bf21d7a5f0388
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2024-06-18 12:26:45 -0400

    0.3.5

--- acore_soap_app/api.py
+++ acore_soap_app/api.py
@@ -8,3 +8,7 @@ from .agent.api import SOAPRequest
 from .agent.api import SOAPResponse
 from .sdk.api import run_soap_command
 from .sdk.api import canned
+from .exc import EC2IsNotRunningError
+from .exc import RunCommandError
+from .exc import SOAPResponseParseError
+from .exc import SOAPCommandFailedError
--- tests/test_api.py
+++ tests/test_api.py
@@ -5,6 +5,26 @@ from acore_soap_app import api
 
 def test():
     _ = api
+    _ = api.DEFAULT_USERNAME
+    _ = api.DEFAULT_PASSWORD
+    _ = api.DEFAULT_HOST
+    _ = api.DEFAULT_PORT
+    _ = api.SOAPRequest
+    _ = api.SOAPResponse
+    _ = api.run_soap_command
+    _ = api.canned
+    _ = api.canned.extract_online_players
+    _ = api.canned.get_online_players
+    _ = api.canned.is_server_online
+    _ = api.canned.create_account
+    _ = api.canned.set_gm_level
+    _ = api.canned.set_password
+    _ = api.canned.delete_account
+    _ = api.canned.gm_list
+    _ = api.EC2IsNotRunningError
+    _ = api.RunCommandError
+    _ = api.SOAPResponseParseError
+    _ = api.SOAPCommandFailedError
 
 
 if __name__ == "__main__":
